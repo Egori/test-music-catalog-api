a94e2acd56d92abfac60812e3a6939a081148863
Egor Yakimov <ea.yakimov@gmail.com>
2024-10-11 11:49:26 +0300

    add indexes to db

--- migrations/000001_songs.up.sql
+++ migrations/000001_songs.up.sql
@@ -8,3 +8,11 @@ CREATE TABLE IF NOT EXISTS songs (
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
 );
+
+-- Уникальный индекс для предотвращения дублирования песен
+CREATE UNIQUE INDEX idx_group_title ON songs (group_name, title);
+
+-- Индексы для ускорения запросов
+CREATE INDEX idx_group_name ON songs (group_name);
+CREATE INDEX idx_title ON songs (title);
+CREATE INDEX idx_release_date ON songs (release_date);
